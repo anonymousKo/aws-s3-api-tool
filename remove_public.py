fdf703bfd9874da697ad2bb07bb6456812befbb0
koresuki <1789151097@qq.com>
2021-07-21 13:50:31 +0800

    delete personal info

--- remove_public.py
+++ remove_public.py
@@ -8,8 +8,8 @@ import boto3
 
 BUCKET = sys.argv[1]
 PATH = sys.argv[2]
-s3client = boto3.client("s3",aws_access_key_id="AKIAU56KBRH3J6M7LRAN",
-    aws_secret_access_key="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",region_name="ap-east-1")
+s3client = boto3.client("s3",aws_access_key_id="aws_access_key_id",
+    aws_secret_access_key="aws_secret_access_key",region_name="ap-east-1")
 paginator = s3client.get_paginator('list_objects_v2')
 page_iterator = paginator.paginate(Bucket=BUCKET, Prefix=PATH)
 for page in page_iterator:
